9af2ea15a61203b3f7fae5b2dcb4a00c3ada8784
Hemant <hemant.ajax@gmail.com>
2013-02-11 16:18:38 +0530

    shims and path setting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,18 @@
 require.config({
   shim: {
+  	backbone:{
+  		deps:["../components/underscore/underscore", "jquery"],
+  		exports: "Backbone"
+  	}
   },
 
   paths: {
-    jquery: 'vendor/jquery.min'
+    jquery: 'vendor/jquery.min',
+    backbone: "../components/backbone/backbone"
   }
 });
  
-require(['app'], function(app) {
+require(['backbone'], function(app) {
   // use app here
-  console.log(app);
+  console.log(Backbone);
 });
