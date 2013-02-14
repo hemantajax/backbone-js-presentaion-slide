9e3cc4304a6500dd7dd6c10b57676082e79549f2
Hemant <hemant.ajax@gmail.com>
2013-02-14 17:56:24 +0530

    added git basic commands to slide

--- app/slides.js
+++ app/slides.js
@@ -2,8 +2,8 @@ window.slides = [
 				{title: "1st slide"},
 				{image: "http://placekitten.com/2600/300"},
 				{	
-					title: "why to add a bullet",
-					bullets: ["bullet one", "bullet 2", "bullet 3", "bullet 4"]},
+					title: "How to use git",
+					bullets: ["git init", "git status", "git add .", "git commit -m \"my 1st commit\" ", "git log", "sign in or create your GitHub account.", "git remot­e add origi­n git@g­ithub.com:­hemantajax­/try_git.g­it" , "git push -u origi­n maste­r", "Pulling Remotely: git pull origi­n maste­r", "git diff HEAD", "git diff --sta­ged", "Resetting the Stage: git reset­ octof­amily/octo­dog.txt", "Undo: git check­out -- octoc­at.txt", "Create new branch: git branch clean_up", "Switching Branches: git check­out clean­_up", "Removing All The Things: git rm \"*.txt\"", "Commiting Branch Changes: Commiting Branch Changes", "Switching Back to master: git checkout master", "Preapring to merge: git merge clean_up", "Removing branch: clean branch -d clean_up", "The final push: git push"]},
 				{
 					quote: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The 	point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using",
 					cite: "Hemant Singh"
